93fd8510df38645cb451232633f90f7ff4c59bfe
Rony <ronysilvati@live.com>
2018-10-06 03:34:04 +0000

    Commit initial

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import movies from './movies';
+import users from './users';
+import profiles from './profiles';
+
+
+/*
+Display a list of movies where each movie contains a list of users that favorited it.
+
+For detailed instructions, refer to instructions.md.
+*/
+
+
+
+
+
+
+class App extends Component {
+  
+  constructor(props){
+  	const props = super.props;
+    const movies = 
+    
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <h2>How Popular is Your Favorite Movie?</h2>
+		{
+				
+		}
+      </div>
+    );
+  }
+}
+
+export default App;
